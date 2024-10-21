aa778eb1d2a6b893b29b56828052e7b624208962
vidhu <vidhu.krishnan@experionmea.com>
2024-10-21 19:02:05 +0530

    logout changes

--- src/Components/Layout/MenuIcons.js
+++ src/Components/Layout/MenuIcons.js
@@ -61,11 +61,24 @@ const LogoutItem = styled.li`
   position: absolute;
   bottom: 0;
   
-  width: 80px; /* Reduced width */
-  left: 10px; /* Center it within the sidebar */
+  width: 150px; 
+  left: 25px; 
 
   &:hover {
-    background-color: rgba(255, 255, 255, 0.1); /* Simple hover background */
+    background-color: rgba(255, 255, 255, 0.1);
+   
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.2); 
+  }
+
+  i {
+    margin-right: 10px;
+    
+    transition: transform 0.4s, color 0.4s; 
+  }
+
+  &:hover i {
+    transform: scale(1.15); 
+    color: #ffcc00; 
   }
 `;
 
