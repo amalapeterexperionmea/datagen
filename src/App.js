d40ba323090c047fb8e857d2fd2f379bbbf2c8bf
HALFCELESTIAL <aswinhere111@gmail.com>
2024-10-22 16:18:31 +0530

    datagen prblm 2 commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Data_generation from './Components/Data_generation/Data_generation';
 import Layout from './Components/Layout/Layout';
 import LoginPage from './Components/Login/Loginpage';
 import User from './Components/User/User';
