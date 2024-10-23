d6ae9d8e11b33c2de43fc70273d946814476d3af
Reshma <reshma.raj@ilearningengines.com>
2024-10-23 16:43:31 +0530

    update-list

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
-// import Data_generation from './Components/Data_generation/Data_generation';
-// import Layout from './Components/Layout/Layout';
-// import LoginPage from './Components/Login/Loginpage';
-// import User from './Components/User/User';
-// import Client from './Components/Client/client'; 
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import UserTable from './Components/User/UserTable';
-// import Form from './Components/Client/clientreg';
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <div >
-//         <Routes>
-//           <Route path="" element={<LoginPage />} />
-//           <Route path="/layout" element={<Layout />} />
-//           <Route path="/data-generation"  element ={<Data_generation/>}/>
-//           <Route path="/user" element={<User />} />
-//           <Route path="/client" element={<Client/>}/>
-//           <Route path="/userlist" element={<UserTable />} />
-//           <Route path="/form" element={<Layout><Form /></Layout>} />
-          
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-import Data_generation from './Pages/Generate/Data_generation'; // Corrected path
-import Layout from './Layout/Layout'; // Corrected path
-import LoginPage from './Pages/Login/Loginpage'; // Corrected path
-import User from './Pages/User/User'; // Corrected path
-import Client from './Pages/Client/client'; // Corrected path
-import UserTable from './Pages/User/UserTable'; // Corrected path
-import Form from './Pages/Client/clientreg'; // Corrected path
+import Data_generation from './Pages/Generate/Data_generation'; 
+import Layout from './Layout/Layout'; 
+import LoginPage from './Pages/Login/Loginpage'; 
+import User from './Pages/User/User'; 
+import Client from './Pages/Client/client'; 
+import UserTable from './Pages/User/UserTable'; 
+import Form from './Pages/Client/clientreg'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Update from './Pages/Client/updateclient';
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<LoginPage />} /> {/* Updated default path */}
+          <Route path="/" element={<LoginPage />} /> 
           <Route path="/layout" element={<Layout />} />
           <Route path="/data-generation" element={<Data_generation />} />
           <Route path="/user" element={<User />} />
           <Route path="/client" element={<Client />} />
           <Route path="/userlist" element={<UserTable />} />
+          <Route path="/update" element={<Layout><Update /></Layout>} />
           <Route path="/form" element={<Layout><Form /></Layout>} />
         </Routes>
       </div>
