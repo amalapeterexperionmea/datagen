36efd494c5836cc8b072ce18001413b36ff1d6b4
Reshma <reshma.raj@ilearningengines.com>
2024-11-04 13:07:13 +0530

    style of container

--- src/Components/Search/SearchBar.js
+++ src/Components/Search/SearchBar.js
@@ -37,7 +37,7 @@ const DropdownContainer = styled.div`
   width: 93%; 
   height:135px;
   margin-left:42px;
-  margin-top:70px;
+  margin-top:90px;
 `;
 
 const SearchBar = ({ onSearch,onToggleFilter }) => {
