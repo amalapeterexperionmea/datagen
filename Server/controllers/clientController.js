1671a2f276bd9085d96b5e812bff874cd420e43e
vidhu <vidhu.krishnan@experionmea.com>
2024-11-21 11:31:11 +0530

    addclient update

--- Server/controllers/clientController.js
+++ Server/controllers/clientController.js
@@ -1,19 +1,20 @@
 const Client = require('../models/Client');
 
 exports.addclient = async (req, res) => {
-    const { name,shortName,domain,postgres,mongodb } = req.body;
-  
-    try {
-      const existingClient = await Client.findOne({ name });
-      if (!existingClient || existingClient.shortName !== shortName) {
-        const newClient  = new Client({ name, shortName, domain, postgres, mongodb});
-        await newClient.save(); 
-        res.status(201).json({ message: 'Client added successfully!'  });
-      }
-      
-       res.status(401).json({ message: 'client already exists!' });
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ message: 'Internal server error', details: error.message });
+  const { name, shortName, domain, postgres, mongodb } = req.body;
+
+  try {
+    const existingClient = await Client.findOne({ name });
+    if (!existingClient || existingClient.shortName !== shortName) {
+      const newClient = new Client({ name, shortName, domain, postgres, mongodb });
+      await newClient.save();
+      return res.status(201).json({ message: 'User added successfully!' });
     }
-  };
+
+   
+    return res.status(401).json({ message: 'client already exists!' });
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ message: 'Internal server error', details: error.message });
+  }
+};
